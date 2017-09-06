f98c1dbec0fd9adff9f1ef5a1d64fea96d28367c
yangzhihui <luozhenyu336>
2017-09-06 22:26:18 +0800

    demo基本完事，进入泛型视图

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.template import loader
 from django.http import Http404
 from .models import Question, Choice
+from django.views import generic
 
 from django.shortcuts import render, get_object_or_404
 
@@ -17,6 +18,12 @@ def index(request):
     }
     return render(request, 'polls/index.html', context);
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
 def detail(request, question_id):
 
     question = get_object_or_404(Question, pk = question_id)
@@ -26,14 +33,25 @@ def detail(request, question_id):
     #     raise Http404('Question does not exist')
     return render(request, 'polls/detail.html', {'question' : question})
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/results.html', {'question' : question})
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     try:
         select_choice = question.choice_set.get(pk = request.POST['choice'])
+
     except (KeyError, Choice.DoesNotExist):
         return render(request,
                       'polls/detail.html',
